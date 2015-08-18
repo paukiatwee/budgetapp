e85c2c0f8534e1ed5ae5e167a05db427fbada411
paukiatwee <paukiatwee@gmail.com>
2015-08-18 23:01:24 +0800

    remove unused code

--- src/main/resources/app/assets/js/app.js
+++ src/main/resources/app/assets/js/app.js
@@ -1,21 +1,3 @@
-var spinnerOpts = {
-  lines: 13, // The number of lines to draw
-  length: 20, // The length of each line
-  width: 10, // The line thickness
-  radius: 30, // The radius of the inner circle
-  corners: 1, // Corner roundness (0..1)
-  rotate: 0, // The rotation offset
-  direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#000', // #rgb or #rrggbb or array of colors
-  speed: 1, // Rounds per second
-  trail: 60, // Afterglow percentage
-  shadow: false, // Whether to render a shadow
-  hwaccel: false, // Whether to use hardware acceleration
-  className: 'spinner', // The CSS class to assign to the spinner
-  zIndex: 2e9, // The z-index (defaults to 2000000000)
-  top: '50%', // Top position relative to parent
-  left: '50%' // Left position relative to parent
-};
 
 var budgetApp = angular.module('budgetApp', ['ngRoute', 'ngAnimate', 'wu.masonry', 'ui.bootstrap', 'angular-google-analytics', 'angular-loading-bar', 'angularMoment', 'financeControllers', 'financeServices']);
 
@@ -254,7 +236,7 @@ angular.module('budgetApp').directive('chart', function() {
   return {
     restrict: 'E',
     link: function(scope, elem, attrs) {
-      var options = scope[attrs.ngOptions] || {};
+      var options = scope[attrs.chartOptions] || {};
       scope.$watch(attrs.ngModel, function (data) {
         if (!data) { return; }
         $.plot(elem, data, options);
