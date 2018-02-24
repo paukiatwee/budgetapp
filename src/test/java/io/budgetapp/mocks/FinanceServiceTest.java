41dd84286ce5d2881cdd4fe7a5bf74b5093e859b
Nathan Chao <n2chao@gmail.com>
2018-02-24 17:37:33 -0500

    add the addUserExistingUserNameTest test

--- src/test/java/io/budgetapp/mocks/FinanceServiceTest.java
+++ src/test/java/io/budgetapp/mocks/FinanceServiceTest.java
@@ -1,35 +1,61 @@
 package io.budgetapp.mocks;
 
+import io.budgetapp.application.DataConstraintException;
 import io.budgetapp.crypto.PasswordEncoder;
 import io.budgetapp.dao.*;
+import io.budgetapp.model.User;
 import io.budgetapp.model.form.SignUpForm;
 import io.budgetapp.service.FinanceService;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Optional;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class FinanceServiceTest {
 
-    @Test
-    public void addUserTest(){
+    private UserDAO userDAOMock;
+    private BudgetDAO budgetDAOMock;
+    private BudgetTypeDAO budgetTypeDAOMock;
+    private CategoryDAO categoryDAOMock;
+    private TransactionDAO transactionDAOMock;
+    private RecurringDAO recurringDAOMock;
+    private AuthTokenDAO authTokenDAOMock;
+    private PasswordEncoder passwordEncoderMock;
+    private SignUpForm signUpFormMock;
 
-        UserDAO userDAOMock = mock(UserDAO.class);
-        BudgetDAO budgetDAOMock = mock(BudgetDAO.class);
-        BudgetTypeDAO budgetTypeDAOMock = mock(BudgetTypeDAO.class);
-        CategoryDAO categoryDAOMock = mock(CategoryDAO.class);
-        TransactionDAO transactionDAOMock = mock(TransactionDAO.class);
-        RecurringDAO recurringDAOMock = mock(RecurringDAO.class);
-        AuthTokenDAO authTokenDAOMock = mock(AuthTokenDAO.class);
-        PasswordEncoder passwordEncoderMock = mock(PasswordEncoder.class);
+    @Before
+    public void initMocks(){
+        userDAOMock = mock(UserDAO.class);
+        budgetDAOMock = mock(BudgetDAO.class);
+        budgetTypeDAOMock = mock(BudgetTypeDAO.class);
+        categoryDAOMock = mock(CategoryDAO.class);
+        transactionDAOMock = mock(TransactionDAO.class);
+        recurringDAOMock = mock(RecurringDAO.class);
+        authTokenDAOMock = mock(AuthTokenDAO.class);
+        passwordEncoderMock = mock(PasswordEncoder.class);
+        signUpFormMock = mock(SignUpForm.class);
+    }
 
-        SignUpForm signUpForm = mock(SignUpForm.class);
 
+    //verifies that an exception is caught when adding a user with an existing username
+    @Test(expected=DataConstraintException.class)
+    public void addUserExistingUserNameTest(){
+        //given
         FinanceService financeService = new FinanceService(userDAOMock, budgetDAOMock, budgetTypeDAOMock, categoryDAOMock, transactionDAOMock, recurringDAOMock, authTokenDAOMock, passwordEncoderMock);
+        User userMock = mock(User.class);
+        Optional<User> userOptional = Optional.of(userMock);
+        SignUpForm form = new SignUpForm();
+        form.setUsername("usernameExists");
+        form.setPassword("password");
+        when(userDAOMock.findByUsername("usernameExists")).thenReturn(userOptional);
 
-        financeService.addUser(signUpForm);
+        //when
+        financeService.addUser(form);
 
-        //add verification
-
-        
+        //then exception is caught via the @Test annotation
     }
 
 }
